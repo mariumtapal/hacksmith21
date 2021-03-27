8f1175f64e3ca241f7a239b3c9458732e8c114d9
mariumtapal <mariumtapal@gmail.com>
2021-03-26 22:53:22 -0400

    cleaning data + app

--- app.R
+++ app.R
@@ -1,7 +1,21 @@
 library(shiny)
+library(reactable)
+
 ui <- fluidPage(
-  "Hello, world!"
+  reactableOutput("table")
 )
-server <- function(input, output, session) {
+
+server <- function(input, output) {
+  output$table <- renderReactable({
+    reactable(vaccines_am2, searchable = TRUE, filterable = TRUE, theme = reactableTheme(
+      borderColor = "#dfe2e5",
+      stripedColor = "#f6f8fa",
+      highlightColor = "#f0f5f9",
+      cellPadding = "8px 12px",
+      style = list(fontFamily = "-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif"),
+      searchInputStyle = list(width = "100%")
+    ))
+  })
 }
+
 shinyApp(ui, server)
